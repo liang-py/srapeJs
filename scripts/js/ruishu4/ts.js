d87144595be29bb7d5e024353480dc3cade45772
lianggaochao <17638161636@163.com>
2023-04-13 17:41:28 +0800

    get window

---
+++ scripts/js/ruishu4/ts.js
@@ -0,0 +1,69 @@
+// 生成window对象的$_ts
+
+var eval_js = ""
+
+window = {
+    $_ts: {},
+    // 为什么添加data
+    eval: function (data) {
+        eval_js = data
+    },
+    document: {},
+//     localStorage: {
+//     "FSSBB22": "466975:2647",
+//     "FSSBB48": "466833:1",
+//     "FSSBB43": "466994:2",
+//     "FSSBB3": "466975:mkl4B3pd.bIfLLkp5TSWoG",
+//     "FSSBB24": "466833:6",
+//     "$_fh0": "ME8CVTpv2TBOiYLWNZts6ucTlM0",
+//     "FSSBB46": "466977:2",
+//     "FSSBB52": "466994:1",
+//     "FSSBB90": "466830:1",
+//     "$_fb": "eUjYZ38c56UCr3Zxp0YFMKPQmlPFd19y0Js4dJtGnu9m8szBZhM5b1_nLNb9G.HD",
+//     "FSSBB17": "466975:LUDV_JcV9BV8vB8w5oohfG",
+//     "$_YWTU": "eNP0Exw0t8sKxwP4EJS2z0zlMb0rE48YCbTYDyHaF0a",
+//     "$_f1": "Dw3Au.0k5f2nKwZPxHMUiHhe47Q",
+//     "$_f0": "bPDdOo9zc_ruy.7NU5ZzQJvtX80",
+//     "__#classType": "localStorage",
+//     "FSSBB45": "466977:1",
+//     "$_nd": "987999",
+//     "FSSBB93": "466830:1",
+//     "FSSBB94": "466975:1",
+//     "FSSBB23": "466833:975",
+//     "FSSBB2": "466975:fvuhsRi6N3Q72uJbEJLMwa",
+//     "FSSBB42": "466977:2",
+//     "FSSBB18": "466975:WCaXmJIhM_ptB9k4GtGgDa",
+//     "$_cDro": "383",
+//     "$_ck": "LB5Ch7ydOsPjafifFwgToa"
+// }
+}
+
+location = {
+    "ancestorOrigins": {},
+    "href": "http://www.fangdi.com.cn/new_house/new_house_detail.html",
+    "origin": "http://www.fangdi.com.cn",
+    "protocol": "http:",
+    "host": "www.fangdi.com.cn",
+    "hostname": "www.fangdi.com.cn",
+    "port": "",
+    "pathname": "/new_house/new_house_detail.html",
+    "search": "",
+    "hash": ""
+}
+
+document = {
+    "scripts": ["script", "script"]
+}
+
+
+function $_ts(f_js, iffe){
+    f_js;
+    iffe;
+    console.log(window.$_ts)
+    console.log(eval_js)
+    return window.$_ts
+}
+// console.log(window)
+// console.log(eval_js)
+// console.log(Object.keys(window.$_ts))
+// console.log(window.$_ts)
