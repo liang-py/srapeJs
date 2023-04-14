f33f0924062cdceecc008e95670cf9927cdf7376
lianggaochao <17638161636@163.com>
2023-04-14 17:43:13 +0800

    ts 赋值替换

--- module/ruishu.py
+++ module/ruishu.py
@@ -3,6 +3,8 @@ import re
 import json
 from utils.CookieJar import cookies_items
 from urllib.parse import urljoin
+from pyquery import PyQuery as pq
+import execjs
 
 
 class RuiShu:
@@ -36,9 +38,73 @@ class RuiShu:
         # 获取content
         self.content = re.findall(r'<meta content="(.*?)">', resp.text)[0]
         src = re.findall(r'src="(.*?)" r=', resp.text)[0]
-        # 请求js文件
+        doc = pq(resp.text)
+        # 202 自执行代码
+        iffe_data = doc.find('script')[1].text
+        # js外链
         src_url = urljoin(self.host, src)
-        self.session.get(src_url)
+        src_data = self.session.get(src_url).text
+        # 获取ts和虚拟机代码
+        add_js = """   
+           // 生成window对象的$_ts
+            
+            var eval_js = ""
+            
+            window = {
+                $_ts: {},
+                // 为什么添加data
+                eval: function (data) {
+                    eval_js = data
+                }
+            }
+            
+            location = {
+                "ancestorOrigins": {},
+                "href": "http://www.fangdi.com.cn/new_house/new_house_detail.html",
+                "origin": "http://www.fangdi.com.cn",
+                "protocol": "http:",
+                "host": "www.fangdi.com.cn",
+                "hostname": "www.fangdi.com.cn",
+                "port": "",
+                "pathname": "/new_house/new_house_detail.html",
+                "search": "",
+                "hash": ""
+            }
+            
+            document = {
+                "scripts": ["script", "script"]
+            }
+            function ts(f_js, iffe){
+                eval(f_js);
+                eval(iffe);
+                console.log(window.$_ts)
+                return [window.$_ts, eval_js]
+            
+            }
+        """
+        ctx = execjs.compile(add_js)
+        ts, exec_js = ctx.call('ts', src_data, iffe_data)
+        print(ts)
+        print(exec_js)
+        # 获取20位核心代码的后四位
+        ts_4_variable = re.findall(r'\[29\]\]\(\[(.*?)\]\)', exec_js)[0].split(',')
+        ts_name = ts_4_variable[0].split('.')[0].split('$')[1]
+        # 取开头20个赋值语句
+        ts_20_function = re.findall(r'(_\$%s\._\$\S{2}=_\$\S{2};)' % ts_name, exec_js)
+        print(ts_4_variable, ts_20_function)
+        ts_4_variable_index = []
+        for ts_4 in ts_4_variable:
+            # 获取ts中的函数名
+            func_name = ts[ts_4.split('.')[1]]
+            for t in ts_20_function:
+                if func_name in t:
+                    ts_4_variable_index.append(ts_20_function.index(t))
+        print(ts_4_variable_index)
+        base_index = ['_$bJ', '_$zr', '_$Sf', '_$B0', '_$pV', '_$Ns', '_$Lm', '_$Ub', '_$pn', '_$Yz', '_$7b', '_$U6', '_$BW', '_$mw', '_$ry', '_$LZ', '_$ZE', '_$9j', '_$25']
+        # 替换js执行文件
+        with open('../scripts/js/ruishu4/iffe.js', 'r', encoding='utf8') as f:
+            f_js = f.read()
+
         res = cookies_items(self.session.cookies.items())
         print(res)
 
---
+++ test.py
@@ -0,0 +1,25 @@
+ts_str = """
+_$Iv._$bJ = _$fs;
+    _$Iv._$zr = _$BJ;
+    _$Iv._$Sf = _$Do;
+    _$Iv._$B0 = _$3K;
+    _$Iv._$pV = _$JU;
+    _$Iv._$Ns = _$B8;
+    _$Iv._$Lm = _$dR;
+    _$Iv._$Ub = _$I_;
+    _$Iv._$pn = _$m6;
+    _$Iv._$Yz = _$cJ;
+    _$Iv._$7b = _$G4;
+    _$Iv._$U6 = _$mx;
+    _$Iv._$BW = _$Tk;
+    _$Iv._$mw = _$AW;
+    _$Iv._$ry = _$vD;
+    _$Iv._$LZ = _$rS;
+    _$Iv._$ZE = _$K$;
+    _$Iv._$9j = _$DL;
+    _$Iv._$25 = _$Q0;
+"""
+index_func = []
+for t in ts_str.replace(' ', '').split(';')[:-1]:
+    index_func.append(t.split('=')[0].split('.')[1])
+print(index_func)
