c1d404d4cef43bd4fc848877c43cfec2a5a4dd96
glig <xyhm_l@163.com>
2023-04-24 21:05:02 +0800

    4/24 GM

--- plugins/service.js
+++ plugins/service.js
@@ -5,33 +5,32 @@ var sdk_JSL = require('../scripts/js/jsl/jsl.js')
 var bodyParser = require('body-parser')
 const port = 3000; // 设置服务端口
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/GM/Header', (req,res)=>{
     // var data = req.data
     // console.log(data)
     headers = sdk_GM.getHeader()
-    res.send(headers)
-})
+    res.send(headers);
+});
 
-app.post('/DM/encData', (req,res)=>{
-    var data = req.data
-    console.log(data)
-    encData = sdk_JSL.Cookie(data)
+app.post('/GM/encData', (req,res)=>{
+    var data = req.body
+    console.log(req.body)
+    encData = sdk_GM.getEnc(data)
     res.send(encData)
 })
 
 app.get('/cookie', (req,res)=>{
     var data = req.data;
-    decData = sdk_GM.getDec(data);
+    decData = sdk_JSL.Cookie(data);
     res.send(decData);
 })
 
 app.get('/', (req, res) => {
     res.send('hello world')
 });
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
 
 app.listen(port, () => {
     console.log(`app listen on port ${port}`)
